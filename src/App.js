51cfc701022e90f14158e0b3096c2871abb78e63
Shadowmaster <shadowmaster.sm@yandex.ru>
2021-04-01 14:39:09 +0300

    Dummy main grid created

--- src/App.js
+++ src/App.js
@@ -3,21 +3,47 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app-wrapper">
+      <header className='header'>
+        <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/Spb_metro_logo.svg/600px-Spb_metro_logo.svg.png' alt="logo" />
       </header>
+      <nav className='nav'>
+        <div>
+          <a>Profile</a>
+        </div>
+        <div>
+          <a>Messages</a>
+        </div>
+        <div>
+          <a>News</a>
+        </div>
+        <div>
+          <a>Music</a>
+        </div>
+        <div>
+          <a>Settings</a>
+        </div>
+      </nav>
+      <div className='content'>
+        <div>
+            <img src="https://image.freepik.com/free-photo/pic-du-midi-ossau-and-ayous-lake-in-the-french-pyrenees-mountains_112793-9123.jpg"/>
+        </div>
+        <div>
+            ava + desc
+        </div>
+        <div>
+            Posts
+            <div>
+                New Post
+            </div>
+            <div>
+                Post 1
+            </div>
+            <div>
+                Post 2
+            </div>
+        </div>
+      </div>
     </div>
   );
 }
