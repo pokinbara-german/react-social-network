d78676d1743d448c3e9cb3299fef5d42c23e6b80
Shadowmaster <shadowmaster.sm@yandex.ru>
2021-04-06 23:16:39 +0300

    Messages component

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -5,11 +5,36 @@
  */
 import React from 'react';
 import styles from './Messages.module.css';
+import {NavLink} from 'react-router-dom';
+
+const DialogsItem = (props) => {
+    let path = "/messages/" + props.id;
+    return(
+            <div className={styles.dialog}>
+                    <NavLink to={path}>{props.name}</NavLink>
+                </div>
+           );
+}
+
+const Message = (props) => {
+    return(
+            <div className={styles.message}>{props.message}</div>
+           );
+}
 
 const Messages = () => {
     return(
-        <div>
-            Messages
+        <div className={styles.dialogs}>
+            <div className={styles.dialogs_items}>
+                <DialogsItem id="1" name="Andrey"/>
+                <DialogsItem id="2" name="Sergey"/>
+                <DialogsItem id="3" name="Misha"/>
+            </div>
+            <div className={styles.messages}>
+                <Message message="First!"/>
+                <Message message="Second!"/>
+                <Message message="Third!"/>
+            </div>
         </div>);
 };
 
