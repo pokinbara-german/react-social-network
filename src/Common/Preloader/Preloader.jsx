d85b0668bfbdc0d260571b8713e4b2313f2ca389
Shadowmaster <shadowmaster.sm@yandex.ru>
2021-05-12 23:33:40 +0300

    Fix profileinfo style and param for preloader

--- src/Common/Preloader/Preloader.jsx
+++ src/Common/Preloader/Preloader.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import styles from './Preloader.module.css';
 import preloader from "../../assets/images/preloader-spinner.svg";
 
-const Preloader = () => {
+const Preloader = (props) => {
     return (
-        <div className={styles.preloaderWrapper}>
+        <div className={!props.notCentered ? styles.preloaderWrapper : null}>
             <img alt='preloader' className={styles.preloader} src={preloader}/>
         </div>
     );
