41369ea0c00ac9108116bfd79f015f043df3b2d3
Shadowmaster <shadowmaster.sm@yandex.ru>
2021-05-19 18:58:17 +0300

    Added new button in AdditionalInfo.jsx

--- src/components/Profile/ProfileInfo/AdditionalInfo/AdditionalInfo.jsx
+++ src/components/Profile/ProfileInfo/AdditionalInfo/AdditionalInfo.jsx
@@ -1,9 +1,10 @@
-import styles from "./AdditionalInfo.module.css";
 import React from "react";
+import styles from "./AdditionalInfo.module.css";
 
 const AdditionalInfo = (props) => {
     return(
         <div className={styles.additionalInfo}>
+            {props.isOwner && <button onClick={props.setEditMode}>Редактировать</button>}
             <div><span>Обо мне: </span>{props.aboutMe || 'Пусто'}</div>
             <div><span>Ищет работу: </span>{props.lookingForAJob ? 'Да' : 'Нет'}</div>
             {props.lookingForAJob && <div><span>Навыки: </span>{props.lookingForAJobDescription}</div>}
