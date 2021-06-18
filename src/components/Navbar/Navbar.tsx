07c380844a2d544e559c699fb78837d0b59cf113
Shadowmaster <shadowmaster.sm@yandex.ru>
2021-06-18 22:53:12 +0300

    Add navbar items auto-selection on load.

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,30 +5,32 @@
  */
 import React from 'react';
 import styles from './Navbar.module.css';
-import {NavLink} from 'react-router-dom';
+import {NavLink, useHistory} from 'react-router-dom';
 import {Menu} from 'antd';
 import {ProfileOutlined, UserOutlined, MessageOutlined, BarsOutlined, CustomerServiceOutlined, ControlOutlined} from '@ant-design/icons';
 
 const Navbar = () => {
+    const history = useHistory();
+
     return(
-        <Menu theme="light" mode='inline' defaultSelectedKeys={['1']} className={styles.nav}>
-            <Menu.Item key="1" icon={<ProfileOutlined/>}>
-                <NavLink to="/profile" activeClassName={styles.active}>Profile</NavLink>
+        <Menu theme='light' mode='inline' defaultSelectedKeys={[history.location.pathname]} className={styles.nav}>
+            <Menu.Item key='/profile' icon={<ProfileOutlined/>}>
+                <NavLink to='/profile' activeClassName={styles.active}>Profile</NavLink>
             </Menu.Item>
-            <Menu.Item key="2" icon={<MessageOutlined/>}>
-                <NavLink to="/messages" activeClassName={styles.active}>Messages</NavLink>
+            <Menu.Item key='/messages' icon={<MessageOutlined/>}>
+                <NavLink to='/messages' activeClassName={styles.active}>Messages</NavLink>
             </Menu.Item>
-            <Menu.Item key="3" icon={<BarsOutlined/>}>
+            <Menu.Item key='/news' icon={<BarsOutlined/>}>
                 <NavLink to="/news" activeClassName={styles.active}>News</NavLink>
             </Menu.Item>
-            <Menu.Item key="4" icon={<CustomerServiceOutlined/>}>
-                <NavLink to="/music" activeClassName={styles.active}>Music</NavLink>
+            <Menu.Item key='/music' icon={<CustomerServiceOutlined/>}>
+                <NavLink to='/music' activeClassName={styles.active}>Music</NavLink>
             </Menu.Item>
-            <Menu.Item key="5" icon={<UserOutlined/>}>
-                <NavLink to="/users" activeClassName={styles.active}>Users</NavLink>
+            <Menu.Item key='/users' icon={<UserOutlined/>}>
+                <NavLink to='/users' activeClassName={styles.active}>Users</NavLink>
             </Menu.Item>
-            <Menu.Item key="6" icon={<ControlOutlined/>}>
-                <NavLink to="/settings" activeClassName={styles.active}>Settings</NavLink>
+            <Menu.Item key='/settings' icon={<ControlOutlined/>}>
+                <NavLink to='/settings' activeClassName={styles.active}>Settings</NavLink>
             </Menu.Item>
         </Menu>
     );
