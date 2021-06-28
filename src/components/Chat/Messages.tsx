b064a9ad9ae78db3b14bc01a4cda225832a8299d
Shadowmaster <shadowmaster.sm@yandex.ru>
2021-06-28 19:51:03 +0300

    Add chat autoscroll autoset\unset.

--- src/components/Chat/Messages.tsx
+++ src/components/Chat/Messages.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {messageType} from '../API/chat-api';
 import {useSelector} from 'react-redux';
 import {getChatMessages} from '../../Common/Selectors/Selectors';
@@ -6,21 +6,38 @@ import {getChatMessages} from '../../Common/Selectors/Selectors';
 export const Messages: React.FC = () => {
     const messages = useSelector(getChatMessages);
     const messagesRef = useRef<HTMLDivElement>(null);
+    const [isAutoscroll, setIsAutoscroll] = useState(true);
 
     useEffect(() => {
-        setTimeout(() => {
-            messagesRef.current?.scrollIntoView({behavior: 'smooth', block: 'end'});
-        }, 500);
-    }, [messages]);
+        if (isAutoscroll) {
+            setTimeout(() => {
+                messagesRef.current?.scrollIntoView({behavior: 'smooth', block: 'end'});
+            }, 500);
+        }
+    }, [messages, isAutoscroll]);
+
+    /**
+     * Detects scroll end and set autoscroll to true.
+     * @param event
+     */
+    function scrollHandler(event: React.UIEvent<HTMLDivElement, UIEvent>) {
+        const element = event.currentTarget;
+
+        if ((element.scrollHeight - element.scrollTop) === element.clientHeight) {
+            !isAutoscroll && setIsAutoscroll(true);
+        } else {
+            isAutoscroll && setIsAutoscroll(false);
+        }
+    }
 
     return (
-        <div style={{height: '74vh', overflowY: 'auto'}}>
+        <div style={{height: '74vh', overflowY: 'auto'}} onScroll={scrollHandler}>
             {messages.map((messageItem, index) => <Message key={index} message={messageItem}/>)}
             <div  ref={messagesRef}/>
         </div>
     );
 }
-const Message: React.FC<{ message: messageType }> = ({message}) => {
+const Message: React.FC<{ message: messageType }> = React.memo(({message}) => {
     return (
         <div>
             <div><img alt={'ava'} src={message.photo}/>{message.userName}</div>
@@ -28,4 +45,4 @@ const Message: React.FC<{ message: messageType }> = ({message}) => {
             <hr/>
         </div>
     );
-}
+});
