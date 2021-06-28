08fff61a89ad3770632443b2ec5c866dd84c96d5
Shadowmaster <shadowmaster.sm@yandex.ru>
2021-06-28 19:21:36 +0300

    Add chat autoscroll to newest.

--- src/components/Chat/Messages.tsx
+++ src/components/Chat/Messages.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import {messageType} from '../API/chat-api';
 import {useSelector} from 'react-redux';
 import {getChatMessages} from '../../Common/Selectors/Selectors';
 
 export const Messages: React.FC = () => {
     const messages = useSelector(getChatMessages);
+    const messagesRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        setTimeout(() => {
+            messagesRef.current?.scrollIntoView({behavior: 'smooth', block: 'end'});
+        }, 500);
+    }, [messages]);
 
     return (
         <div style={{height: '74vh', overflowY: 'auto'}}>
             {messages.map((messageItem, index) => <Message key={index} message={messageItem}/>)}
+            <div  ref={messagesRef}/>
         </div>
     );
 }
