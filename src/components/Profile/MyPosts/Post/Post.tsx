da1814841e38861afcd5258c0956babcdb65c1f3
Shadowmaster <shadowmaster.sm@yandex.ru>
2021-08-22 15:51:05 +0300

    New styles for profile-posts.

--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -13,7 +13,7 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import ListItemText from '@material-ui/core/ListItemText';
 import Tooltip from '@material-ui/core/Tooltip';
-import Divider from '@material-ui/core/Divider';
+import Card from '@material-ui/core/Card';
 import ThumbUpIcon from '@material-ui/icons/ThumbUp';
 import ThumbUpOutlinedIcon from '@material-ui/icons/ThumbUpOutlined';
 import {createStyles, makeStyles, Theme} from '@material-ui/core';
@@ -32,31 +32,41 @@ type likesBlockPropsType = {
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
-        root: {
+        likeWrapper: {
             display: 'flex',
             '& > *': {
                 margin: theme.spacing(1),
             },
+        },
+        postWrapper: {
+            display: 'flex',
+            margin: theme.spacing(1),
+        },
+        text: {
+            whiteSpace: 'pre-line',
+            overflowWrap: 'break-word',
         }
     }),
 );
 
 const Post: React.FC<postPropsType> = (props) => {
+    const classes = useStyles();
     let avatarSmall = props.avatar || userMale;
 
-    const actions = [<LikesBlock postId={props.postId} likeCount={props.likeCount} key={props.postId}/>];
+    const actions = [
+        <LikesBlock postId={props.postId} likeCount={props.likeCount} key={props.postId}/>
+    ];
 
     return(
-        <React.Fragment>
+        <Card variant={'outlined'} className={classes.postWrapper}>
             <ListItem alignItems="flex-start">
                 <ListItemAvatar>
                     <Avatar alt="ava" src={avatarSmall} />
                 </ListItemAvatar>
-                <ListItemText primary={props.message} secondary={actions}>
+                <ListItemText className={classes.text} primary={props.message} secondary={actions}>
                 </ListItemText>
             </ListItem>
-            <Divider component="li" />
-        </React.Fragment>
+        </Card>
     );
 };
 
@@ -84,7 +94,7 @@ const LikesBlock: React.FC<likesBlockPropsType> = (props) => {
     };
 
     return(
-        <span onClick={likeHandler} id={'post-' + props.postId} className={classes.root}>
+        <span onClick={likeHandler} id={'post-' + props.postId} className={classes.likeWrapper}>
             <Tooltip arrow title='Like' aria-label='like'>
                 {props.likeCount ? <ThumbUpIcon fontSize={iconSize}/> : <ThumbUpOutlinedIcon fontSize={iconSize}/>}
             </Tooltip>
