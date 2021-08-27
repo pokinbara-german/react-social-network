882d70bb593962a65ba9a07ac785b9c0b0cc0d79
Shadowmaster <shadowmaster.sm@yandex.ru>
2021-08-27 18:19:52 +0300

    AddMessage stylization with Material-UI.

--- src/components/Chat/AddMessage.tsx
+++ src/components/Chat/AddMessage.tsx
@@ -2,13 +2,39 @@ import React, {ChangeEvent, useState} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {sendMessage} from '../../reducers/chatReducer';
 import {appStateType} from '../../redux/reduxStore';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Tooltip from '@material-ui/core/Tooltip';
+import {createStyles, makeStyles, Theme} from '@material-ui/core/styles';
 
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        chatForm: {
+            display: 'flex',
+            flexDirection: 'column',
+            margin: theme.spacing(1),
+            width: '40ch',
+            '& > *': {
+                margin: theme.spacing(1),
+            },
+        },
+        sendButton: {
+            alignSelf: 'start'
+        }
+    }),
+);
+
+/**
+ * Returns form for send message to chat.
+ * @constructor
+ */
 export const AddMessage: React.FC = () => {
+    const classes = useStyles();
+    const dispatch = useDispatch();
+
     const [message, setMessage] = useState('');
 
     const isConnected = useSelector((state: appStateType) => state.chat.isConnected);
-
-    const dispatch = useDispatch();
 
     function sendMessageHandler() {
         if (!message) {
@@ -24,13 +50,18 @@ export const AddMessage: React.FC = () => {
     }
 
     return (
-        <div>
-            <div>
-                <textarea onChange={messageChange} value={message}/>
-            </div>
-            <div>
-                <button disabled={!isConnected} onClick={sendMessageHandler}>Send</button>
-            </div>
+        <div className={classes.chatForm}>
+            <Tooltip title={'You can type multiline. Just hit enter.'} aria-label='Hint' placement="right" arrow>
+                <TextField multiline onChange={messageChange} value={message} placeholder='Type something'/>
+            </Tooltip>
+            <Button className={classes.sendButton}
+                    variant='contained'
+                    color='primary'
+                    disabled={!isConnected}
+                    onClick={sendMessageHandler}
+            >
+                Send
+            </Button>
         </div>
     );
 }
