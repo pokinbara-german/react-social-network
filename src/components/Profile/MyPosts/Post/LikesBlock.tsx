85df64e0212ff14f5460a7400359f6c5cacaf2c0
Shadowmaster <shadowmaster.sm@yandex.ru>
2021-09-01 17:39:51 +0300

    Move LikesBlock to different file.

---
+++ src/components/Profile/MyPosts/Post/LikesBlock.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import {useDispatch} from 'react-redux';
+import {profileActions} from '../../../../reducers/profileReducer';
+import Tooltip from '@material-ui/core/Tooltip';
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+import ThumbUpOutlinedIcon from '@material-ui/icons/ThumbUpOutlined';
+import {createStyles, makeStyles, Theme} from '@material-ui/core';
+
+type likesBlockPropsType = {
+    postId: string,
+    likeCount: number
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        likeWrapper: {
+            display: 'flex',
+            '& > *': {
+                margin: theme.spacing(1),
+            },
+        }
+    }),
+);
+
+/**
+ * Returns counter for post likes with tooltip and button to add like.
+ * @param {likesBlockPropsType} props
+ * @constructor
+ */
+export const LikesBlock: React.FC<likesBlockPropsType> = (props) => {
+    const iconSize = 'small';
+    const dispatch = useDispatch();
+    const classes = useStyles();
+
+    /**
+     * Add like to post
+     * @param {React.MouseEvent<HTMLSpanElement>} event - sets automatically, not need to put it
+     */
+    const likeHandler = (event: React.MouseEvent<HTMLSpanElement>) => {
+        let targetId = event.currentTarget.id;
+        let postId = targetId.substr(5);
+
+        if (postId) {
+            dispatch(profileActions.addLike(postId));
+        }
+    };
+
+    return (
+        <span onClick={likeHandler} id={'post-' + props.postId} className={classes.likeWrapper}>
+            <Tooltip arrow title='Like' aria-label='like'>
+                {props.likeCount ? <ThumbUpIcon fontSize={iconSize}/> : <ThumbUpOutlinedIcon fontSize={iconSize}/>}
+            </Tooltip>
+            <span>{props.likeCount}</span>
+        </span>
+    );
+};
--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -6,17 +6,14 @@
 import React from 'react';
 import userMale from "../../../../assets/images/user-male.png";
 import {stringOrNull} from '../../../../types';
-import {useDispatch} from 'react-redux';
-import {profileActions} from '../../../../reducers/profileReducer';
 import Avatar from '@material-ui/core/Avatar';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import ListItemText from '@material-ui/core/ListItemText';
-import Tooltip from '@material-ui/core/Tooltip';
 import Card from '@material-ui/core/Card';
-import ThumbUpIcon from '@material-ui/icons/ThumbUp';
-import ThumbUpOutlinedIcon from '@material-ui/icons/ThumbUpOutlined';
 import {createStyles, makeStyles, Theme} from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
+import {LikesBlock} from './LikesBlock';
 
 type postPropsType = {
     postId: string,
@@ -25,19 +22,8 @@ type postPropsType = {
     avatar: stringOrNull
 }
 
-type likesBlockPropsType = {
-    postId: string,
-    likeCount: number
-}
-
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
-        likeWrapper: {
-            display: 'flex',
-            '& > *': {
-                margin: theme.spacing(1),
-            },
-        },
         postWrapper: {
             display: 'flex',
             margin: theme.spacing(1),
@@ -67,39 +53,6 @@ const Post: React.FC<postPropsType> = (props) => {
                 </ListItemText>
             </ListItem>
         </Card>
-    );
-};
-
-/**
- * Returns counter for post likes with tooltip and button to add like.
- * @param {likesBlockPropsType} props
- * @constructor
- */
-const LikesBlock: React.FC<likesBlockPropsType> = (props) => {
-    const iconSize = 'small';
-    const dispatch = useDispatch();
-    const classes = useStyles();
-
-    /**
-     * Add like to post
-     * @param {React.MouseEvent<HTMLSpanElement>} event - sets automatically, not need to put it
-     */
-    const likeHandler = (event: React.MouseEvent<HTMLSpanElement>) => {
-        let targetId = event.currentTarget.id;
-        let postId = targetId.substr(5);
-
-        if (postId) {
-            dispatch(profileActions.addLike(postId));
-        }
-    };
-
-    return(
-        <span onClick={likeHandler} id={'post-' + props.postId} className={classes.likeWrapper}>
-            <Tooltip arrow title='Like' aria-label='like'>
-                {props.likeCount ? <ThumbUpIcon fontSize={iconSize}/> : <ThumbUpOutlinedIcon fontSize={iconSize}/>}
-            </Tooltip>
-            <span>{props.likeCount}</span>
-        </span>
     );
 };
 
