9cf68f1d75a7d445a2e2b5c0fff164067fdd289f
Shadowmaster <shadowmaster.sm@yandex.ru>
2021-09-23 18:58:33 +0300

    Updates

--- static/js/6.f87382bb.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[6],{313:function(p,s,t){"use strict";t.r(s);t(0);var c=t(2);s.default=function(){return Object(c.jsx)("div",{children:"Music"})}}}]);
-//# sourceMappingURL=6.f87382bb.chunk.js.map
